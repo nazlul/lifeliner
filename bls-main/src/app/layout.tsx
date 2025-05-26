48fc90d43b8951468ed39f0a31ca0c4a9d66eb82
Nazlul Rizan <eng.nazrizan@gmail.com>
2025-05-26 15:38:43 +0530

    Meta Pixels Added

--- bls-main/src/app/layout.tsx
+++ bls-main/src/app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   description: "BLS Training Campaign",
   icons: {
     icon: "/logo.png",
-  }
+  },
 };
 
 export default function RootLayout({
@@ -16,11 +16,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '569513825157564');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=569513825157564&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
