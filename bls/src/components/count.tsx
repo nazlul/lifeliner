dc36588d65ac60332e55e047e3549c70f7d8b386
Nazlul Rizan <eng.nazrizan@gmail.com>
2025-12-01 11:41:08 +0530

    updated events

--- bls/src/components/count.tsx
+++ bls/src/components/count.tsx
@@ -12,9 +12,9 @@ interface Stat {
 
 export default function StatsRow() {
   const stats: Stat[] = [
-    { icon: '/count/1.svg', number: 12763, label: 'People trained' },
-    { icon: '/count/2.svg', number: 14469, label: 'Pledges Taken' },
-    { icon: '/count/3.svg', number: 181, label: 'Sessions Organized' },
+    { icon: '/count/1.svg', number: 14632, label: 'People trained' },
+    { icon: '/count/2.svg', number: 16762, label: 'Pledges Taken' },
+    { icon: '/count/3.svg', number: 209, label: 'Sessions Organized' },
   ]
 
   return (
